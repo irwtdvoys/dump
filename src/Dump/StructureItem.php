32ec15b4a70ad750d45de5231aa102d1e0c36d41
Steve Adams <irwtdvoys@gmail.com>
2018-02-15 16:26:19 +0000

    Rename Dump class to StructureItem in the Dump namespace

--- src/Dump/StructureItem.php
+++ src/Dump/StructureItem.php
@@ -1,7 +1,7 @@
 <?php
-	namespace Cruxoft;
+	namespace Cruxoft\Dump;
 
-	class Dump
+	class StructureItem
 	{
 		public $type;
 		public $value;
@@ -57,93 +57,90 @@
 			return $output;
 		}
 
-		public static function structure($object, $level = 0)
+		public function __construct($object = null, $level = 0)
 		{
 			$type = gettype($object);
 
-			$item = new Dump();
-			$item->level = $level;
+			$this->level = $level;
 
 			switch ($type)
 			{
 				case "boolean":
-					$item->type = $type;
-					$item->value = ($object === true) ? "true" : "false";
+					$this->type = $type;
+					$this->value = ($object === true) ? "true" : "false";
 					break;
 				case "integer":
-					$item->type = $type;
-					$item->value = $object;
+					$this->type = $type;
+					$this->value = $object;
 					break;
 				case "double":
-					$item->type = "float";
-					$item->value = $object;
+					$this->type = "float";
+					$this->value = $object;
 					break;
 				case "string":
-					$item->type = $type . "(" . strlen($object) . ")";
-					$item->value = "\"" . (string)$object . "\"";
+					$this->type = $type . "(" . strlen($object) . ")";
+					$this->value = "\"" . (string)$object . "\"";
 					break;
 				case "array":
-					$item->type = $type . "(" . count($object) . ")";
-					$item->children = array();
+					$this->type = $type . "(" . count($object) . ")";
+					$this->children = array();
 
 					foreach ($object as $key => $value)
 					{
-						$child = self::structure($value, ($level + 1));
+						$child = new StructureItem($value, ($level + 1));
 						$child->isChild = true;
-						$item->children[$key] = $child;
+						$this->children[$key] = $child;
 					}
 
 					break;
 				case "object":
 					$class = get_class($object);
-					$item->type = ($class === "stdClass") ? $type : "class";
+					$this->type = ($class === "stdClass") ? $type : "class";
 
 					if ($class !== "stdClass")
 					{
-						$item->value = $class;
+						$this->value = $class;
 					}
 
-					$item->children = array();
+					$this->children = array();
 
 					foreach ($object as $key => $value)
 					{
-						$child = self::structure($value, ($level + 1));
+						$child = new StructureItem($value, ($level + 1));
 						$child->isChild = true;
-						$item->children[$key] = $child;
+						$this->children[$key] = $child;
 					}
 
 					break;
 				case "resource":
-					$item->type = $type;
-					$item->value = get_resource_type($object);
+					$this->type = $type;
+					$this->value = get_resource_type($object);
 
-					$resources = get_resources($item->value);
+					$resources = get_resources($this->value);
 
 					foreach ($resources as $key => $resource)
 					{
 						if ($object === $resource)
 						{
-							$item->type .= "(" . $key . ")";
+							$this->type .= "(" . $key . ")";
 							break;
 						}
 					}
 
-					if ($item->value === "stream")
+					if ($this->value === "stream")
 					{
 						$meta = stream_get_meta_data($object);
-						$item->value .= "-" . $meta['stream_type'];
+						$this->value .= "-" . $meta['stream_type'];
 					}
 
 					break;
 				case "NULL":
-					$item->type = "null";
+					$this->type = "null";
 					break;
 				default:
-					$item->type = "unknown";
+					$this->type = "unknown";
 					break;
 			}
-
-			return $item;
 		}
 	}
 ?>
